681ae0b69bf0ffd21d5f94d630adef7039c5b2d6
Franco Daneri <fdaneri@fi.uba.ar>
2026-08-26 21:11:19 +0000

    Se actualiza 'consolidar.sh'

--- consolidar.sh
+++ consolidar.sh
@@ -1,4 +1,9 @@
 echo "Corriendo------"
+if [ ! -f "$HOME/EPNro1/Salida/FILENAME.txt" ]; then
+    echo "Archivo FILENAME.txt no encontrado. Creando archivo..."
+    touch "$HOME/EPNro1/Salida/FILENAME.txt"
+    echo "Archivo FILENAME.txt creado con éxito."
+fi
 while true; do
     echo "El proceso de consolidación está activo."
     sleep 5 #Simula un trabajo que se ejecuta cada 5 segundos 
